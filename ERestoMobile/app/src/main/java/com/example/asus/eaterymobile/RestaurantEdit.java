8534b8b6e01b3c25491e0c8a2c39ff2af7f3d41a
kusumo <shirokamiyuuto@gmail.com>
2017-12-18 14:57:28 +0700

    restaurant side update

---
+++ ERestoMobile/app/src/main/java/com/example/asus/eaterymobile/RestaurantEdit.java
@@ -0,0 +1,77 @@
+package com.example.asus.eaterymobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.*;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by hari sabardi on 17/12/2017.
+ */
+
+public class RestaurantEdit extends AppCompatActivity implements NavigationView.
+        OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_edit);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout)
+                findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)
+                findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_changemenu){
+            // startActivity(new Intent(getApplicationContext(), changemenu.class));
+        }
+        else if (id == R.id.nav_restaurantprofile) {
+            //   startActivity(new Intent(getApplicationContext(), restaurantprofile.class));
+        }
+        else if (id == R.id.nav_setting) {
+
+        }
+        else if (id == R.id.nav_logout) {
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
+
+        return true;
+    }
+
+}
---
+++ ERestoMobile/app/src/main/java/com/example/asus/eaterymobile/RestaurantMenuEdit.java
@@ -0,0 +1,8 @@
+package com.example.asus.eaterymobile;
+
+/**
+ * Created by hari sabardi on 18/12/2017.
+ */
+
+public class RestaurantMenuEdit {
+}
