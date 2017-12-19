511cccd1433df4e24da25941309c18f54451fb71
kusumo <shirokamiyuuto@gmail.com>
2017-12-19 15:21:15 +0700

    update

--- ERestoMobile/app/src/main/java/com/example/asus/eaterymobile/RestaurantMenuEdit.java
+++ ERestoMobile/app/src/main/java/com/example/asus/eaterymobile/RestaurantMenuEdit.java
@@ -1,8 +1,41 @@
 package com.example.asus.eaterymobile;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.*;
+import android.view.MenuItem;
+
 /**
  * Created by hari sabardi on 18/12/2017.
  */
 
-public class RestaurantMenuEdit {
+public class RestaurantMenuEdit extends AppCompatActivity implements NavigationView.
+        OnNavigationItemSelectedListener{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_menu_edit);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout)
+                findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)
+                findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
 }
