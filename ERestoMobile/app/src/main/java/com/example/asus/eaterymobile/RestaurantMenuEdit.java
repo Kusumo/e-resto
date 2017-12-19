0989deaf8bfb7b4db72c9144f844125202ee8e96
kusumo <shirokamiyuuto@gmail.com>
2017-12-19 21:38:33 +0700

    restaurant menu edit

--- ERestoMobile/app/src/main/java/com/example/asus/eaterymobile/RestaurantMenuEdit.java
+++ ERestoMobile/app/src/main/java/com/example/asus/eaterymobile/RestaurantMenuEdit.java
@@ -1,13 +1,16 @@
 package com.example.asus.eaterymobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.*;
+import android.view.Menu;
 import android.view.MenuItem;
 
 /**
@@ -36,6 +39,37 @@ public class RestaurantMenuEdit extends AppCompatActivity implements NavigationV
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
+        int id = item.getItemId();
+        if(id == R.id.nav_changemenu){
+            // startActivity(new Intent(getApplicationContext(), changemenu.class));
+        }
+        else if (id == R.id.nav_restaurantprofile) {
+            //   startActivity(new Intent(getApplicationContext(), restaurantprofile.class));
+        }
+        else if (id == R.id.nav_setting) {
+
+        }
+        else if (id == R.id.nav_logout) {
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
